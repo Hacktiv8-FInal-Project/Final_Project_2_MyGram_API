7680a4d1a5462f42b4dbaf4a5a9ba799bda8801d
Dani Rizky Zaelani <122273503+DaniRizkyZaelani@users.noreply.github.com>
2023-11-18 09:14:09 +0700

    Update config.js

--- config/config.js
+++ config/config.js
@@ -1,6 +1,6 @@
 const dotenv = require('dotenv')
 const path = require('path')
-console.log(dotenv.config({ path: path.join(__dirname, '../.env') }))
+//console.log(dotenv.config({ path: path.join(__dirname, '../.env') }))//
 
 const config = {
   "development": {
